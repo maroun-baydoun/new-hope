6c05200d5b8d7db185ebf7c405e7f413e9c73168
Maroun Baydoun <maroun.baydoun@gmail.com>
2021-02-06 01:24:24 +0100

    Add content

--- src/index.js
+++ src/index.js
@@ -1,2 +1,4 @@
 import "./scss/reset.scss";
 import "./scss/index.scss";
+
+import "./prismjs";
--- src/variables.js
+++ src/variables.js
@@ -1,8 +1,8 @@
 module.exports = {
-  title: "",
-  description: "",
-  heading1: "",
-  heading2: "",
-  githubLink: "",
-  npmLink: "",
+  title: "new-hope - Esperanto tokenizer",
+  description: "Esperanto tokenizer written in TypeScript",
+  heading1: "new-hope",
+  heading2: "Esperanto tokenizer",
+  githubLink: "https://github.com/maroun-baydoun/new-hope",
+  npmLink: "https://www.npmjs.com/package/new-hope",
 };
