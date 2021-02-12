25a43166fa23f63352ec49278f82c43c426e2e92
Maroun Baydoun <maroun.baydoun@gmail.com>
2021-02-13 00:58:51 +0100

    Update demo placement

--- src/variables.js
+++ src/variables.js
@@ -1,6 +1,6 @@
 module.exports = {
   title: "new-hope - Esperanto tokenizer",
-  description: "Esperanto tokenizer written in TypeScript",
+  description: "new-hope is an Esperanto tokenizer written in TypeScript and available on npm",
   heading1: "new-hope",
   heading2: "Esperanto tokenizer",
   githubLink: "https://github.com/maroun-baydoun/new-hope",
